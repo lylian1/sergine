2e09dab95657cb689add763cf9b0c04a0b7da16a
lylian krizoua <krizoualylian@gmail.com>
2021-06-15 15:24:51 +0200

    product

--- app/controllers/produits_controller.rb
+++ app/controllers/produits_controller.rb
@@ -1,2 +1,17 @@
 class ProduitsController < ApplicationController
+
+   def show
+   @produit = Produit.find(params[:id])
+    end
+
+
+    def new
+        @produits = Produit.new
+    end
+    
+    def create 
+        @produit = Produit.new
+        @produit = Produit.save
+    end
+
 end
