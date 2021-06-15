3b15404420fc979b72b5461090f4e9981fcdc91b
lylian krizoua <krizoualylian@gmail.com>
2021-06-15 15:09:43 +0200

    cout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-    def user_params
-         params.require(:user).permit(:first_name, :last_name)
-       end
+    #def user_params
+         #params.require(:user).permit(:first_name, :last_name)
+       #end
       
 end
